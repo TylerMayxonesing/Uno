03c45437f3b6dba498aa72ad65de742c25cce50a
rayngan999 <rayngan999@gmail.com>
2019-11-10 18:05:18 -0800

    took out enum but have error 55

--- Rules.cpp
+++ Rules.cpp
@@ -10,86 +10,52 @@
 
 
 int Rules::startingHandSize(int starting_hand) {
-    while (true) {
-        std::cout << "StartingHandSize : ";
-        std::cin >> starting_hand;
-        if (starting_hand <= 1) {
-            std::cout << "Please enter value greater than 1" << std::endl;
-            continue;
-        } else {
             return starting_hand;}
-    }
-}
 
 int Rules::unoCallOutPenalty(int uno_penalty) {
-    while (true) {
-        std::cout << "UnoCalloutPenalty : ";
-        std::cin >> uno_penalty;
-        if (uno_penalty <= 0) {
-            std::cout << "Please enter value greater than 0" << std::endl;
-            continue;
-    } else {return uno_penalty;}
+    return uno_penalty;
 }
-}
+
 
 int Rules::badUnoCalloutPenalty(int bad_uno_penalty) {
-    while (true) {
-        std::cout << "BadUnoCalloutPenalty : ";
-        std::cin >> bad_uno_penalty;
-        if (bad_uno_penalty <= 0) {
-            std::cout << "Please enter value greater than 0" << std::endl;
-            continue;
-        } else {return bad_uno_penalty;}
-    }
-}
+    return bad_uno_penalty;}
+
 
 int Rules::drawLimit(int max_draw) {
-    while (true) {
-        std::cout<<"DrawLimit : ";
-        std::cin>>max_draw;
-        if (max_draw <= 1) {
-            std::cout << "Please enter value greater than 1" << std::endl;
-            continue;
-        }
-        else if(max_draw == 0) {
+        if(max_draw == 'N') {
             return 0;
         }
         else {return max_draw;}
         }
-    }
 
 
-int Rules::mustPlayCardEachTurn() {
-    std::cout<<"DrawLimit : ";
-    char input;
 
-    enum level must_play;
-    std::cin>>input;
-    if (input == 't'){
-        must_play = static_cast<level>('T');
+int Rules::mustPlayCardEachTurn(char must_play) {
+
+
+
+    if (must_play == 't'){
+        return 1;
 //        std::cout<<"it is true";
     }
-    else if (input =='f') {
-      must_play = static_cast<level>('F');
+    else if (must_play =='f') {
+        return 0;
 //        std::cout<<"it is false";
     }
 
-    return 0;
 }
 
-int Rules::renegingfunc() {
-    std::cout<<"Reneging : ";
-    char input;
-    std::cin>>input;
-    //std::cin>>reneging;
-    enum level reneging;
-    if (input == 'a'){
-        reneging = static_cast<level>('A');
+int Rules::renegingFunction(char reneging) {
+//    enum level reneging;
+    if (reneging == 'A'){
+        return 0;
+//        reneging = static_cast<level>('A');
     }
-    if (input == 'l'){
-        reneging = static_cast<level>('L');
+    if (reneging == 'L'){
+        return 1;
+//        reneging = static_cast<level>('L');
     }
-    return 0;
+
 }
 
 void Rules::readfile(int starting_hand,int uno_penalty,int bad_uno_penalty,int max_draw, char must_play, char reneging) {
@@ -113,7 +79,6 @@ void Rules::readfile(int starting_hand,int uno_penalty,int bad_uno_penalty,int m
 
         for(auto& s: tokens)
             if (s == "StartingHandSize"){starting_hand = num;}
-
         for(auto& s: tokens)
             if (s == "UnoCalloutPenalty"){uno_penalty = num;}
         for(auto& s: tokens)
@@ -124,9 +89,15 @@ void Rules::readfile(int starting_hand,int uno_penalty,int bad_uno_penalty,int m
             if (s == "MustPlayCardEachTurn"){must_play = num;}
         for(auto& s: tokens)
             if (s == "Reneging"){reneging = num;}
-
-
     }
+
+    Rules::startingHandSize(0);
+    Rules::unoCallOutPenalty(0);
+    Rules::badUnoCalloutPenalty(0);
+    Rules::drawLimit(0);
+    Rules::mustPlayCardEachTurn(0);
+    Rules::renegingFunction(0);
+
 
 
 }
