d74f9d4f8a7cb19cbe80434061b8ebed68a9de4d
rayngan999 <rayngan999@gmail.com>
2019-11-12 18:43:06 -0800

    ADDED discard pile class

--- DiscardPile.cpp
+++ DiscardPile.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "DiscardPile.h"
+
+DiscardPile::DiscardPile(std::vector <Card*> DiscardPile) {
+    aDiscardPile = DiscardPile;
+}
+
+std::vector<Card *> DiscardPile::getDiscardPile() {
+    return aDiscardPile;
+}
+
+void DiscardPile::makeDiscardPile(std::vector<Card *> DiscardPile) {
+    aDiscardPile = DiscardPile;
+}
